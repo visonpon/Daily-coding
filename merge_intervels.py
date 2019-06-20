860bbb548d70ebb19bcaef3691c2d9bf7c2ad122
visonpon <visonpon@gmail.com>
2019-06-21 07:50:39 +0800

    Update merge_intervels.py

--- merge_intervels.py
+++ merge_intervels.py
@@ -1,12 +1,12 @@
 class solution(object):
   def merge(self,intervels):
-    if not intervesl:
+    if not intervels:
       return []
     intervels.sort(key:lambda x : x.start)
     res = [intervels[0]]
     for i in range(1,len(intervels)):
       if intervels[i].start <= res[-1].end
         res[-1].end = max(intervels[i].end,res[-1].end)
-      esle:
+      else:
         res.append(intervels[i])
       return res
