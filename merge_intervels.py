503fe40f986a1d3eae7df83c9030a1ef351bc196
visonpon <visonpon@gmail.com>
2019-06-21 08:06:17 +0800

    Update merge_intervels.py

--- merge_intervels.py
+++ merge_intervels.py
@@ -10,3 +10,25 @@ class solution(object):
       else:
         res.append(intervels[i])
       return res
+    
+    '''
+    insert intervels
+    '''
+    class soultion(object):
+      def insert(self,intervels,newintervels):
+        intervels.append(newintervels)
+        intervels.sort(key:lambda x:x.start)
+        s = len(intervels)
+        
+        res =[]
+        for i in range(s):
+          if res ==[]:
+            res.append(intervels[i])
+          else:
+            if res[-1].start < intervels[i].start <= res[-1].end :
+              res[-1].end = max(intervels[i].end, res[-1].end)
+            else:
+              res.append(res[i])
+        return res
+       
+            
