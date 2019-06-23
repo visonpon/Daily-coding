75e330e51da84c1f9bada04704999fcc96fa7323
visonpon <visonpon@gmail.com>
2019-06-23 15:40:55 +0800

    Update LargestNumber.py

--- LargestNumber.py
+++ LargestNumber.py
@@ -8,3 +8,27 @@ class solution:
     largest=''.join(largest)
     
     return 0 if largest[0]=='0' else largest
+
+  #solution2
+  class solution(object):
+    def smaller(self,a,b):
+      if str(a)+str(b) < str(b)+str(a):
+        return True
+      else:
+        return Fasle
+    
+    def largetnumber(self,num):
+      resultstr=''
+      for i in range(len(num)):
+        for j in range(i+1,len(num)):
+          if self.smaller(num[i],num(j)):
+            tmp = num[i]
+            num[i] = num[j]
+            num[j] = num[i]
+      for i in range(len(num)):
+        resultstr= resultsrt + str(i)
+        
+     if resultstr[0]=='0':
+      return '0'
+     return resultstr
+          
